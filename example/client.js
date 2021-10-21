0fca0c2bb1d4d7dc2a47e3db671a7242b927e2d0
Philipp Schweiger <phips0812@gmail.com>
2021-10-21 08:43:10 +0200

    built NLU client based on API 2.1.0

---
+++ example/client.js
@@ -0,0 +1,13 @@
+console.log('CLIENT SCRIPT -- start');
+
+const endPoint = 'https://webgrpc-nlu-develop.ondewo.com:443';
+const authMetaData = {
+	//Authorization: "<--Your authorization token-->",
+	Authorization: ''
+};
+
+runNLUClientSample(endPoint, authMetaData);
+
+function runNLUClientSample(endPoint, authMetaData){
+	// TODO when there is enough time
+}
