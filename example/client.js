cb9cb98541e7150db0955cdf152258dc455e27bf
Ahmed Hasanovic <ahmed.hasanovic@ondewo.com>
2022-09-09 11:52:34 +0200

    moved build to beginning of release command, and added check commands and push

--- example/client.js
+++ example/client.js
@@ -1,13 +1,1 @@
-console.log('CLIENT SCRIPT -- start');
-
-const endPoint = 'https://webgrpc-nlu-develop.ondewo.com:443';
-const authMetaData = {
-	//Authorization: "<--Your authorization token-->",
-	Authorization: ''
-};
-
-runNLUClientSample(endPoint, authMetaData);
-
-function runNLUClientSample(endPoint, authMetaData){
-	// TODO when there is enough time
-}
+console.log('CLIENT SCRIPT -- WORK IN PROGRESS');
